53dfdaf6206394cfd4f5789cf475540adaaa9794
olalaanderi <yolanda_anderi@teknokrat.ac.id>
2024-06-12 19:52:52 +0700

    master:Upload Folder Dokumentasi dan Program

---
+++ app/src/main/java/com/example/ta_pm_si21cdx_terserah/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ta_pm_si21cdx_terserah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
